485b11c98b86aa3ba45c475bf07f86254ee29a73
hideveloper20 <electronicmade9064@outlook.com>
2020-07-09 17:22:48 -0500

    Protecting all routes

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -28,26 +28,4 @@ const jwtStrategy = new passportJWT.Strategy(jwtOptions, (jwtPayload, next) => {
   }
 });
 
-/* const authFunction = (username, password, done) => {
-  let index = _.findIndex(usuarios, (usuario) => usuario.username === username);
-
-  if (index === -1) {
-    log.info(`Usuario ${username} no existe.`);
-    done(null, false);
-  } else {
-    let hashedPassword = usuarios[index].password;
-    bcrypt.compare(password, hashedPassword, (err, iguales) => {
-      if (iguales) {
-        log.info(`Usuario ${username} completó la autenticación.`);
-        done(null, true);
-      } else {
-        log.info(
-          `Usuario ${username} no completo la autenticación, contraseña incorrecta.`
-        );
-        done(null, false);
-      }
-    }); //Comparo las dos contraseñas
-  }
-}; */
-
 module.exports = jwtStrategy;
--- api/recursos/productos/productos.routes.js
+++ api/recursos/productos/productos.routes.js
@@ -5,25 +5,29 @@ const productEjem = require("../../../databaseArr").productEjem;
 const middValidarProducto = require("./productos.middleware");
 const productsRouter = express.Router();
 const log = require("../../../utils/logger");
-
+const passport = require("passport");
+const jwtAuth = passport.authenticate("jwt", { session: false });
 //Ejemplo usando un arreglo
 
 productsRouter.get("/", (req, res) => {
   res.json(productEjem);
 });
 
-productsRouter.post("/", middValidarProducto.validarPorducto, (req, res) => {
-  let newProduct = req.body;
-  /* if (!newProduct.moneda || !newProduct.precio || !newProduct.titulo) {
-    res.status(400).send("Faltan algunos datos");
-    return;
-  } */
+productsRouter.post(
+  "/",
+  [jwtAuth, middValidarProducto.validarPorducto],
+  (req, res) => {
+    let newProduct = {
+      ...req.body,
+      id: uuidv4(),
+      owner: req.user.username,
+    };
 
-  newProduct.id = uuidv4();
-  productEjem.push(newProduct);
-  log.info("Nuevo producto agregado", newProduct);
-  res.status(201).json(newProduct);
-});
+    productEjem.push(newProduct);
+    log.info("Nuevo producto agregado", newProduct);
+    res.status(201).json(newProduct);
+  }
+);
 
 productsRouter.get("/:id", (req, res) => {
   for (let producto of productEjem) {
@@ -35,36 +39,72 @@ productsRouter.get("/:id", (req, res) => {
   res.status(404).send(`El producto con id ${req.params.id} no existe`);
 });
 
-productsRouter.put("/:id", middValidarProducto.validarPorducto, (req, res) => {
-  let id = req.params.id;
-  let reemplazoProducto = req.body;
+productsRouter.put(
+  "/:id",
+  [jwtAuth, middValidarProducto.validarPorducto],
+  (req, res) => {
+    let reemplazoProducto = {
+      ...req.body,
+      id: req.params.id,
+      owner: req.user.username,
+    };
 
-  let indice = _.findIndex(productEjem, (product) => product.id === id); //Encuentra el indice del array productEjem
+    let indice = _.findIndex(
+      productEjem,
+      (product) => product.id === reemplazoProducto.id
+    ); //Encuentra el indice del array productEjem
 
-  if (indice !== -1) {
-    reemplazoProducto.id = id;
-    productEjem[indice] = reemplazoProducto;
-    log.info(`El producto con id ${id} fue reemplazado`, reemplazoProducto);
-    res.status(200).send(reemplazoProducto);
-  } else {
-    log.warn(`El producto con id ${id} no existe`);
-    res.status(404).send(`El producto con id ${id} no existe`);
+    if (indice !== -1) {
+      if (productEjem[indice].owner !== reemplazoProducto.owner) {
+        log.info(
+          `Usuario ${req.user.username} no es dueño del producto con id ${reemplazoProducto.id}. Dueño real es ${productEjem[indice].owner}.`
+        );
+        res
+          .status(401)
+          .send(
+            `No eres dueño del producto con id ${reemplazoProducto.id}. Solo puedes modificar productos creados por ti.`
+          );
+      } else {
+        productEjem[indice] = reemplazoProducto;
+        log.info(
+          `El producto con id ${reemplazoProducto.id} fue reemplazado`,
+          reemplazoProducto
+        );
+        res.status(200).send(reemplazoProducto);
+      }
+    } else {
+      log.warn(`El producto con id ${reemplazoProducto.id} no existe`);
+      res
+        .status(404)
+        .send(`El producto con id ${reemplazoProducto.id} no existe`);
+    }
   }
-});
+);
 
-productsRouter.delete("/:id", (req, res) => {
+productsRouter.delete("/:id", jwtAuth, (req, res) => {
   let indiceBorrar = _.findIndex(
     productEjem,
     (product) => product.id === req.params.id
-  ); //Encuentra el indice del array productEjem
+  );
   if (indiceBorrar === -1) {
     log.warn(`El producto con id ${req.params.id} no existe`);
     res.status(404).send(`El producto con id ${req.params.id} no existe`);
     return;
   } else {
-    log.warn(`El producto con id ${req.params.id} fue borrado`);
-    let borrado = productEjem.splice(indiceBorrar, 1);
-    res.json(borrado);
+    if (productEjem[indiceBorrar].owner !== req.user.username) {
+      log.info(
+        `Usuario ${req.user.username} no es dueño del producto con id ${productEjem[indiceBorrar].id}. Dueño real es ${productEjem[indiceBorrar].owner}.`
+      );
+      res
+        .status(401)
+        .send(
+          `No eres dueño del producto con id ${productEjem[indiceBorrar].id}. Solo puedes eliminar productos creados por ti.`
+        );
+    } else {
+      log.warn(`El producto con id ${req.params.id} fue borrado`);
+      let borrado = productEjem.splice(indiceBorrar, 1);
+      res.json(borrado);
+    }
   }
 });
 
